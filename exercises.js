b4cc2a4be4818405f12ddc7ead93eb693585e42c
Aneika11 <aneikananton@gmail.com>
2023-01-18 18:39:58 -0500

    lab completed

--- exercises.js
+++ exercises.js
@@ -1,36 +1,80 @@
-// Question 1
-const isUpperCase = () => {
+/* eslint-disable linebreak-style */
 
+const { text } = require("stream/consumers");
+
+// Question 1
+const isUpperCase = (words) => {
+return  words === words.toUpperCase() ? true : false;
 };
 
 // Question 2
-const removeVowels = () => {
-
+const removeVowels = (words) => {
+  let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    return words.map(word => {
+        return word.split('').filter(el => !vowels.includes(el)).join('');
+    });
 };
 
 // Question 3
-const wordCap = () => {
-
+const wordCap = (string) => {
+  let words = string.split(/\s+/);
+  let capitalizedWords = words.map(function(word) {
+      return word[0].toUpperCase() + word.slice(1).toLowerCase();
+  });
+  return capitalizedWords.join(" ");
 };
 
 // Question 4
-const swapCase = () => {
-
+const swapCase = (string) => {
+  let swapped = "";
+  for (let i = 0; i < string.length; i++) {
+    let char = string[i];
+    if (char === char.toUpperCase()) {
+      swapped += char.toLowerCase();
+    } else if (char === char.toLowerCase()) {
+      swapped += char.toUpperCase();
+    } else {
+      swapped += char;
+    }
+  }
+  return swapped;
 };
 
 // Question 5
-const staggeredCase = () => {
-
+const staggeredCase = (string) => {
+  let result = "";
+  let isUpper = true;
+  for (let i = 0; i < string.length; i++) {
+      let char = string[i];
+      if (char.match(/[a-zA-Z]/)) {
+          if (isUpper) {
+              result += char.toUpperCase();
+          } else {
+              result += char.toLowerCase();
+          }
+          isUpper = !isUpper;
+      } else {
+          result += char;
+      }
+  }
+  return result;
 };
 
 // Question 6
-const wordLengths = () => {
+const wordLengths = (sentence) => {
+  if (!sentence) return [];
 
+  let words = sentence.split(" ");
+  let result = [];
+  for (let i = 0; i < words.length; i++) {
+    result.push(`${words[i]} ${words[i].length}`);
+  }
+  return result;
 };
 
 // Question 7
-const searchWord = () => {
-
+const searchWord = (word, string) => {
+return string.replace(/[,?!.]/ig,"").split(" ").filter(el => el.toLowerCase() === word.toLowerCase() ).length
 };
 
 
